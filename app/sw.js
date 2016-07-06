c349b9641580db7433e1689bcb3486a946632bf5
Aaron Bushnell <aaron.bushnell@gmail.com>
2016-07-06 11:22:41 -0400

    Add SW comments

--- app/sw.js
+++ app/sw.js
@@ -7,10 +7,12 @@
     console.log('We have activated successfully!');
   }
 
+  // First the Service Worker is installed
   self.addEventListener('install', function (event) {
     event.waitUntil(doSomething());
   });
 
+  // After installation, the Service Worker gets activated
   self.addEventListener('activate', function (event) {
     event.waitUntil(doSomethingElse());
   });
